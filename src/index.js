99abda2d3ebf12a2a293396b9f2182ffdf4baa03
Amo Moloko <u14284783@tuks.co.za>
2018-07-04 12:44:58 +0200

    SUBSCRIPTIONS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const {Prisma} = require('prisma-binding');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const AuthPayload = require('./resolvers/AuthPayload');
+const Subscription = require('./resolvers/Subscription');
 
 
 
@@ -11,7 +12,8 @@ const AuthPayload = require('./resolvers/AuthPayload');
 const resolvers ={
     Query,
     Mutation,
-    AuthPayload
+    AuthPayload,
+    Subscription,
     }
 
     
